91efda7e200879e9cf56563f7c60974c2a16af49
에치알디 <hrder1987@gmail.com>
2026-02-06 09:13:35 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1249189958-2be09",
+  "appId": "1:739975757436:web:ab986b8d73984eb18013f9",
+  "apiKey": "AIzaSyDbyddFpaSsfzJr21E8UT_J3HmjBXlG-18",
+  "authDomain": "studio-1249189958-2be09.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "739975757436"
+};
